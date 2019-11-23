d8a6abbc2ca9e641030906eb120ac3cb1efbf16d
chason <chasonlsmith@gmail.com>
2019-11-22 21:53:01 -0700

    updated friends.js file for json object friends

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+var friends = [
+
+{
+    "name":"Cool Guy",
+    "photo":"https://m.media-amazon.com/images/M/MV5BNDExMzIzNjk3Nl5BMl5BanBnXkFtZTcwOTE4NDU5OA@@._V1_.jpg",
+    "scores":[
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3
+      ]
+  },
+  {
+    "name":"Lame Guy",
+    "photo":"http://3.bp.blogspot.com/-p7gxGxg-NDw/Tla89IblkTI/AAAAAAAAAEU/EABJlY-eajc/s1600/Nicolas_Cage_-_66%25C3%25A8me_Festival_de_Venise_%2528Mostra%2529.jpg",
+    "scores":[
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+      ]  
+  },
+  {
+    "name":"Perfect Person",
+    "photo":"https://cdn.britannica.com/61/137461-050-BB6C5D80/Brad-Pitt-2008.jpg",
+    "scores":[
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+      ]
+  }
+]
+module.exports = friends;
